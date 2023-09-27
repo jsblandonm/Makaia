a01a96370bafd81a8c5c218aebbc02f5430a212a
Sebastian Blandon <94935847+jsblandonm@users.noreply.github.com>
2023-09-27 09:26:58 -0500

    Add files via upload

---
+++ workshopEstructuraDatos/src/HtmlValidator.java
@@ -0,0 +1,32 @@
+import java.util.Queue;
+import java.util.Stack;
+
+
+public class HtmlValidator {
+
+
+	public static Stack<HtmlTag> isValidHtml(Queue<HtmlTag> tags) {
+
+		Stack<HtmlTag> stack = new Stack<>();
+
+		for (HtmlTag tag : tags) {
+			if (tag.isSelfClosing()) {
+				continue;
+			} else if (!tag.isOpenTag()) {
+				if (!stack.isEmpty()) {
+					if (stack.peek().matches(tag)) {
+						stack.pop();
+					} else {
+						return stack;
+					}
+				} else {
+					return null;
+				}
+			} else if (tag.isOpenTag()) {
+				stack.push(tag);
+			}
+		}
+		return stack;
+	}
+}
+
