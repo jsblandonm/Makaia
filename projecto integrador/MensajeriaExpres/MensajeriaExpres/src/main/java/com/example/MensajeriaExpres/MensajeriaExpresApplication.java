dc344bc882dbc7bffb4462f3ed8dddb515492f77
Jsblandonm <sebasblandonmontoya@gmail.com>
2023-10-14 15:00:38 -0500

    Primer avance

---
+++ projecto integrador/MensajeriaExpres/MensajeriaExpres/src/main/java/com/example/MensajeriaExpres/MensajeriaExpresApplication.java
@@ -0,0 +1,13 @@
+package com.example.MensajeriaExpres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensajeriaExpresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensajeriaExpresApplication.class, args);
+	}
+
+}
---
+++ projecto integrador/MensajeriaExpres/MensajeriaExpres/src/test/java/com/example/MensajeriaExpres/MensajeriaExpresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MensajeriaExpres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensajeriaExpresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
